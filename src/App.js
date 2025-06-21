7cd140e00d4b290dc8352b95cb2657c4079b3769
RZAJC2426 <rzajc666@gmail.com>
2025-06-21 04:00:07 +0200

    changed BrowseRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import MovieDetails from './features/movies/MovieDetails';
 import Home from "./components/Home";
 import About from "./components/About";
