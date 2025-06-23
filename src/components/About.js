6f89942dfb8b25f459b08296accc47851e538be3
RZAJC2426 <rzajc666@gmail.com>
2025-06-23 13:00:41 +0200

    added info about a Movie App

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,21 @@ export default function About() {
     return (
         <div className="page-container">
             <h2> O nas</h2>
-            <p> Jesteśmy pasjonatami kina, którzy stworzyli aplikację, by dzielić się miłością do filmów na całym świecie</p>
+            <p> PL: 
+Jest to aplikacja filmowa, która działa poprzez połączenie się z bazą themoviedb.org. Zostały użyte narzędzia o następujących technologiach:
+React
+Redux
+Formik
+Axios
+Framer-motion
+emailjs
+yup
+css
+Strona główna zawiera menu rozwijalne, gdzie zakładka gatunki rozwija się z listą gatunków filmów, a kontakt prowadzi do formularza utworzonego w formiku wraz z Recaptchą, po wypełnieniu którego wyświetla się komunikat z potwierdzeniem e-mail. 
+Zakładka about zawiera informacje o stronie i jego autora. 
+Strona główna posiada karty plakatów filmów ułożone szeregowo, po wejściu których wyświetla się data premiery, ocena i opis filmu. Jest wyszukiwwarka, gdzie można wpisać konkretny tytuł filmu, a obok niej przy lewej stronie są ułożone trzy przyciski sortujące wg. wyświetleń, ocen czy nadchodzące. 
+Dla ułatwienia przeglądania strony jest przełącznik zmiany motywu na ciemny/jasny. 
+Autor: Robert Zając 2025</p>
         </div>
     );
 }
