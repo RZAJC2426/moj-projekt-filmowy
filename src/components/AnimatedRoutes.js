0141a07b708edf52ee7e07399a94e70b438e52ec
RZAJC2426 <rzajc666@gmail.com>
2025-07-01 05:51:03 +0200

    fix: uproszczony routing bez animacji

--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes,Route, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+/*import { AnimatePresence, motion } from "framer-motion";*/
 import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About";
@@ -10,19 +10,18 @@ import GenrePage from "./GenrePage";
 function AnimatedRoutes(){
     const location = useLocation();
     return (
-        <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<PageWrapper><Home /></PageWrapper>}/>
-                <Route path="/kontakt" element={<PageWrapper><Contact/></PageWrapper>}/>
-                <Route path="/o-nas" element={<PageWrapper><About/></PageWrapper>}/>
-                <Route path="/movie/:id" element={<PageWrapper><MovieDetails/></PageWrapper>}/>
-                <Route path="/gatunek/:genreId" element={<PageWrapper><GenrePage /></PageWrapper>}/>
+                <Route path="/" element={<Home />}/>
+                <Route path="/kontakt" element={<Contact/>}/>
+                <Route path="/o-nas" element={<About/>}/>
+                <Route path="/movie/:id" element={<MovieDetails/>}/>
+                <Route path="/gatunek/:genreId" element={<GenrePage />}/>
             </Routes>
-        </AnimatePresence>
+        
     );
 }
 
-const PageWrapper = ({ children }) => (
+/*const PageWrapper = ({ children }) => (
     <motion.div
     initial={{opacity: 0, y:20}}
     animate={{opacity: 1, y:0 }}
@@ -31,5 +30,5 @@ const PageWrapper = ({ children }) => (
     >
         {children}
     </motion.div>
-);
+);*/
 export default AnimatedRoutes;
