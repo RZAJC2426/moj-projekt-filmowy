21d6ebec506c834d7645d99672f11db67465dfba
RZAJC2426 <rzajc666@gmail.com>
2025-07-12 14:25:00 +0200

    dodanie motion

--- src/components/GenresDropdown.js
+++ src/components/GenresDropdown.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const API = "https://api.themoviedb.org/3";
 const key= process.env.REACT_APP_TMDB_API_KEY;
